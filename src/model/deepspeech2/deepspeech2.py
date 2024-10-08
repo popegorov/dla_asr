cf449ac5c3c0e75aa4e8c899eeafadbb9f4cbdf9
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2024-10-08 14:57:03 +0300

    second commit

---
+++ src/model/deepspeech2/deepspeech2.py
@@ -0,0 +1,104 @@
+from typing import Tuple
+
+import torch
+import torch.nn as nn
+from torch import Tensor
+
+from src.model.deepspeech2.convolution import DeepSpeech2Extractor
+from src.model.deepspeech2.modules import LayerNorm, Linear
+from src.model.deepspeech2.rnn import BNReluRNN
+
+
+class DeepSpeech2(nn.Module):
+    """
+    Deep Speech2 model with configurable encoder and decoder.
+    Paper: https://arxiv.org/abs/1512.02595
+
+    Args:
+        input_dim (int): dimension of input vector
+        num_classes (int): number of classfication
+        rnn_type (str, optional): type of RNN cell (default: gru)
+        num_rnn_layers (int, optional): number of recurrent layers (default: 5)
+        rnn_hidden_dim (int): the number of features in the hidden state `h`
+        dropout_p (float, optional): dropout probability (default: 0.1)
+        bidirectional (bool, optional): if True, becomes a bidirectional encoder (defulat: True)
+        activation (str): type of activation function (default: hardtanh)
+        device (torch.device): device - 'cuda' or 'cpu'
+
+    Inputs: inputs, input_lengths
+        - **inputs**: list of sequences, whose length is the batch size and within which each sequence is list of tokens
+        - **input_lengths**: list of sequence lengths
+
+    Returns: output
+        - **output**: tensor containing the encoded features of the input sequence
+    """
+
+    def __init__(
+        self,
+        n_tokens: int,
+        n_feats: int,
+        rnn_type="gru",
+        num_rnn_layers: int = 5,
+        rnn_hidden_dim: int = 512,
+        dropout_p: float = 0.1,
+        bidirectional: bool = True,
+        device: torch.device = "cuda",
+    ):
+        super(DeepSpeech2, self).__init__()
+        self.device = device
+        self.conv = DeepSpeech2Extractor()
+
+        rnn_output_size = rnn_hidden_dim << 1 if bidirectional else rnn_hidden_dim
+
+        self.num_rnn_layers = num_rnn_layers
+
+        self.rnn_layers = nn.Sequential(
+            BNReluRNN(
+                input_size=self.conv.get_output_dim(n_feats),
+                hidden_state_dim=rnn_hidden_dim,
+                rnn_type=rnn_type,
+                bidirectional=bidirectional,
+                dropout_p=dropout_p,
+            ),
+            *(
+                BNReluRNN(
+                    input_size=rnn_output_size,
+                    hidden_state_dim=rnn_hidden_dim,
+                    rnn_type=rnn_type,
+                    bidirectional=bidirectional,
+                    dropout_p=dropout_p,
+                )
+                for _ in range(self.num_rnn_layers)
+            )
+        )
+
+        self.fc = nn.Sequential(
+            LayerNorm(rnn_output_size),
+            Linear(rnn_output_size, n_tokens, bias=False),
+        )
+
+    def forward(
+        self, spectrogram: Tensor, spectrogram_length: Tensor, **batch
+    ) -> Tuple[Tensor, Tensor]:
+        """
+        Forward propagate a `inputs` for  ctc training.
+
+        Args:
+            inputs (torch.FloatTensor): A input sequence passed to encoder. Typically for inputs this will be a padded
+                `FloatTensor` of size ``(batch, seq_length, dimension)``.
+            input_lengths (torch.LongTensor): The length of input tensor. ``(batch)``
+
+        Returns:
+            (Tensor, Tensor):
+            * predicted_log_prob (torch.FloatTensor)s: Log probability of model predictions.
+            * output_lengths (torch.LongTensor): The length of output tensor ``(batch)``
+        """
+        outputs = self.conv(spectrogram)
+        output_lengths = self.conv._get_sequence_lengths(spectrogram_length)
+        outputs = outputs.permute(2, 0, 1).contiguous()
+
+        for rnn_layer in self.rnn_layers:
+            outputs = rnn_layer(outputs, output_lengths)
+
+        outputs = self.fc(outputs.transpose(0, 1)).log_softmax(dim=-1)
+        return {"log_probs": outputs, "log_probs_length": output_lengths}
---
+++ src/model/deepspeech2/rnn.py
@@ -0,0 +1,64 @@
+import torch.nn as nn
+import torch.nn.functional as F
+from torch import Tensor
+
+
+class BNReluRNN(nn.Module):
+    """
+    Recurrent neural network with batch normalization layer & ReLU activation function.
+
+    Args:
+        input_size (int): size of input
+        hidden_state_dim (int): the number of features in the hidden state `h`
+        rnn_type (str, optional): type of RNN cell (default: gru)
+        bidirectional (bool, optional): if True, becomes a bidirectional encoder (defulat: True)
+        dropout_p (float, optional): dropout probability (default: 0.1)
+
+    Inputs: inputs, input_lengths
+        - **inputs** (batch, time, dim): Tensor containing input vectors
+        - **input_lengths**: Tensor containing containing sequence lengths
+
+    Returns: outputs
+        - **outputs**: Tensor produced by the BNReluRNN module
+    """
+
+    supported_rnns = {
+        "lstm": nn.LSTM,
+        "gru": nn.GRU,
+        "rnn": nn.RNN,
+    }
+
+    def __init__(
+        self,
+        input_size: int,
+        hidden_state_dim: int = 512,
+        rnn_type: str = "gru",
+        bidirectional: bool = True,
+        dropout_p: float = 0.1,
+    ):
+        super(BNReluRNN, self).__init__()
+        self.hidden_state_dim = hidden_state_dim
+        self.batch_norm = nn.BatchNorm1d(input_size)
+        rnn_cell = self.supported_rnns[rnn_type]
+        self.rnn = rnn_cell(
+            input_size=input_size,
+            hidden_size=hidden_state_dim,
+            num_layers=1,
+            bias=True,
+            batch_first=True,
+            dropout=dropout_p,
+            bidirectional=bidirectional,
+        )
+
+    def forward(self, spectrogram: Tensor, spectrogram_length: Tensor, **batch):
+        total_length = spectrogram.size(0)
+        x = F.relu(self.batch_norm(spectrogram.transpose(1, 2)))
+
+        x = x.transpose(1, 2)
+        x = nn.utils.rnn.pack_padded_sequence(
+            x, spectrogram_length.cpu(), enforce_sorted=False
+        )
+        x, _ = self.rnn(x)
+        outputs, _ = nn.utils.rnn.pad_packed_sequence(x, total_length=total_length)
+
+        return outputs
