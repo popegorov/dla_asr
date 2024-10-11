7f09c3ca9aef3d5eee75799a07f0bef708c48ce3
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2024-10-11 21:59:11 +0300

    train all

---
+++ src/transforms/spec_augs/__init__.py
@@ -0,0 +1,1 @@
+from src.transforms.spec_augs.time_mask import TimeMasking
---
+++ src/transforms/spec_augs/time_mask.py
@@ -0,0 +1,13 @@
+import torch
+import torchaudio.transforms
+from torch import Tensor, nn
+
+
+class TimeMasking(nn.Module):
+    def __init__(self, prob, time_mask_param):
+        super().__init__()
+        self._aug = torchaudio.transforms.TimeMasking(time_mask_param)
+        self.prob = prob
+
+    def __call__(self, spectrogram: Tensor):
+        return self._aug(spectrogram) if torch.rand(1) < self.prob else spectrogram
