1645d7fb8487bf25edd348ce0e16b35c7b7d0acc
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2024-10-13 20:50:24 +0300

    final

--- cer_wer_script.py
+++ cer_wer_script.py
@@ -1,4 +1,5 @@
 import numpy as np
+import argparse
 from pathlib import Path
 from src.metrics.utils import calc_cer, calc_wer
 from src.text_encoder.ctc_text_encoder import CTCTextEncoder
@@ -9,7 +10,7 @@ def main(dir_path):
     wer = []
     cer = []
     for path in Path(dir_path).iterdir():
-        if path.suffix == ".json":
+        if path.suffix == ".pth":
             text = read_json(path)
             target_text = CTCTextEncoder.normalize_text(text["text"])
             cer.append(calc_cer(target_text, text["pred_text"]))
@@ -19,5 +20,5 @@ def main(dir_path):
 
 
 if __name__ == "__main__":
-    directory = "data/saved/predictions"
+    directory = "data/saved/prediction/test"
     main(directory)
