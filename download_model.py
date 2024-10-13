b32276fe7de526f4cd865a9e59e245a248a3b44a
Timofey Torbakhov <tatotrbakhov@edu.hse.ru>
2024-10-13 21:35:17 +0300

    added download script

--- download_model.py
+++ download_model.py
@@ -1,4 +1,7 @@
 import gdown
+import os
 
 if __name__ == "__main__":
+    if not os.path.exists('saved/model')
+        os.mkdir('saved/model')
     gdown.download("https://drive.google.com/file/d/13V_IdQI_JCwzngyqAq616E4ckjcFRg7E/view?usp=sharing", "saved/model/best_model.pth")
